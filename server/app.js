2a57c47b000d0739fd10f0eb8baaf604cb58e7f4
Eric <elsayer@msn.com>
2021-01-28 13:46:50 -0700

    bolier plate build out

---
+++ server/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(
+	express.urlencoded({
+		extended: true
+	})
+);
+app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+	console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
